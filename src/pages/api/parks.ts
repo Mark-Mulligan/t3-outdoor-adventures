0994fdc8a5f25fbd5d7f973823516374eb2983ae
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-09-16 20:53:06 -0500

    tailwind on front

--- src/pages/api/parks.ts
+++ src/pages/api/parks.ts
@@ -5,21 +5,23 @@ import { prisma } from '../../server/db/client';
 
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
-    // const result = await prisma.nationalParksData.findMany({ skip: 0, take: 10 });
-    console.log(req.query);
     const page = Number(req.query.page);
     const limit = Number(req.query.limit);
 
-    const offset = (page - 1) * limit;
-    const endIndex = page * limit;
+    try {
+      const allResults = await prisma.nationalParksData.findMany();
+      const offset = (page - 1) * limit;
+      const endIndex = page * limit;
+      const results = allResults.slice(offset, endIndex);
+      const totalCount = allResults.length;
+      const totalPages = Math.ceil(totalCount / limit);
 
-    const allResults = await prisma.nationalParksData.findMany();
-
-    const results = allResults.slice(offset, endIndex);
-    const totalCount = allResults.length;
-    const totalPages = Math.ceil(totalCount / limit);
-
-    res.status(200).json({ results, totalCount, totalPages, currentPage: page });
+      res.status(200).json({ results, totalCount, totalPages, currentPage: page });
+      return;
+    } catch (err) {
+      res.status(500).json({ message: 'Failed to fetch data from database.' });
+      return;
+    }
   }
 };
 
