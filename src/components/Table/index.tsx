aaa6105f9c3ca32daf54462705c589669d31e2f8
Mark-Mulligan <mark.mulligan.jr1@gmail.com>
2022-09-27 15:32:06 -0500

    added more tailwind styles

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { FC } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 interface TableColumn<T> {
   field: keyof T;
@@ -13,38 +13,37 @@ interface IProps<T> {
 
 const Table = <T extends object>({ columns, rows }: IProps<T>) => {
   return (
-    <table className="min-w-full leading-normal">
-      <thead>
-        <tr>
-          {columns.map((column) => {
+    <div className="overflow-x-auto relative">
+      <table className="w-full text-sm text-left text-gray-300">
+        <thead className="text-xs uppercase bg-gray-700">
+          <tr>
+            {columns.map((column) => {
+              return (
+                <th key={uuidv4()} className="px-5 py-3">
+                  {column.headerName}
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row) => {
             return (
-              <th
-                key={uuidv4()}
-                className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
-              >
-                {column.headerName}
-              </th>
+              <tr key={uuidv4()} className="border-b bg-gray-800 border-gray-700">
+                {columns.map((column) => {
+                  const data: any = row[column.field];
+                  return (
+                    <td key={uuidv4()} className="px-5 py-3">
+                      {data}
+                    </td>
+                  );
+                })}
+              </tr>
             );
           })}
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((row) => {
-          return (
-            <tr key={uuidv4()}>
-              {columns.map((column) => {
-                const data: any = row[column.field];
-                return (
-                  <td key={uuidv4()} className="px-5 py-3 border-b border-gray-200 bg-white text-sm">
-                    {data}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
