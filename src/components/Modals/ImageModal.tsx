c1143b3d715ab1339ccd5b0948c6bafaf8bc64d0
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-09-30 13:04:58 -0500

    working on image carousel

--- src/components/Modals/ImageModal.tsx
+++ src/components/Modals/ImageModal.tsx
@@ -1,17 +1,25 @@
 // React
-import { FC, Dispatch, SetStateAction } from "react";
+import { FC, Dispatch, SetStateAction } from 'react';
+
+// Next
+import Image from 'next/image';
+
+// UUID
+import { v4 as uuidv4 } from 'uuid';
 
 // Custom Types
-import { IParkImage } from "../../customTypes/parks";
+import { IParkImage } from '../../customTypes/parks';
 
 interface IProps {
   showModal: boolean;
   setShowModal: Dispatch<SetStateAction<boolean>>;
-  parkImg: IParkImage | undefined;
+  images: IParkImage[] | undefined;
+  selectedImageIndex: number;
+  setSelectedImageIndex: Dispatch<SetStateAction<number>>;
 }
 
-const ImageModal: FC<IProps> = ({ showModal, setShowModal, parkImg }) => {
-  if (!parkImg) {
+const ImageModal: FC<IProps> = ({ showModal, setShowModal, images, selectedImageIndex, setSelectedImageIndex }) => {
+  if (!images || images.length === 0) {
     return null;
   }
 
@@ -19,65 +27,93 @@ const ImageModal: FC<IProps> = ({ showModal, setShowModal, parkImg }) => {
     <div
       id="large-modal"
       tabIndex={-1}
-      className={`overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full bg-black/50 ${
-        showModal ? "" : "hidden"
+      className={`overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full bg-black/70 ${
+        showModal ? '' : 'hidden'
       }`}
     >
-      <div className="mx-auto relative p-4 w-full max-w-4xl h-full flex items-center">
-        <div>
-          <div className="relative rounded-lg shadow bg-gray-700 shrink">
-            <div className="flex justify-between items-center p-5 rounded-t border-b border-gray-600">
-              <h3 className="text-xl font-medium text-white">Large modal</h3>
-              <button
-                onClick={() => setShowModal(false)}
-                type="button"
-                className="text-gray-400 bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-600 hover:text-white"
-                data-modal-toggle="large-modal"
+      <div className="pt-10">
+        <button
+          onClick={() => setShowModal(false)}
+          type="button"
+          style={{ zIndex: 2 }}
+          className="absolute top-3 right-2.5 text-gray-400 bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 hover:text-white"
+          data-modal-toggle="popup-modal"
+        >
+          <svg
+            aria-hidden="true"
+            className="w-8 h-8"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+          <span className="sr-only">Close modal</span>
+        </button>
+
+        <h2 className="text-3xl text-center text-white mb-4">{images[selectedImageIndex]?.title}</h2>
+
+        <div className="relative">
+          {images.map((image, index) => {
+            return (
+              <div
+                key={uuidv4()}
+                className={`${selectedImageIndex === index ? '' : 'hidden'} duration-700 ease-in-out`}
+                style={{ maxWidth: 900, maxHeight: '60%', margin: 'auto' }}
               >
-                <svg
-                  aria-hidden="true"
-                  className="w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span className="sr-only">Close modal</span>
-              </button>
-            </div>
-            <div className="p-6 space-y-6">
-              <p className="text-base leading-relaxed text-gray-400">
-                With less than a month to go before the European Union enacts new consumer privacy laws for its
-                citizens, companies around the world are updating their terms of service agreements to comply.
-              </p>
-              <p className="text-base leading-relaxed text-gray-400">
-                The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is
-                meant to ensure a common set of data rights in the European Union. It requires organizations to notify
-                users as soon as possible of high-risk data breaches that could personally affect them.
-              </p>
-            </div>
-            <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-600">
-              <button
-                data-modal-toggle="large-modal"
-                type="button"
-                className="text-white bg-blue-600 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:bg-blue-700 focus:ring-blue-800"
+                <img src={image.url} className="" alt={image.altText} />
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="relative mt-4 mx-auto" style={{ maxWidth: 900 }}>
+          <button
+            type="button"
+            className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          >
+            <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 text-gray-400 bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 hover:text-white">
+              <svg
+                aria-hidden="true"
+                className="w-10 h-10 sm:w-10 sm:h-10 text-gray:100"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                I accept
-              </button>
-              <button
-                data-modal-toggle="large-modal"
-                type="button"
-                className="text-gray-300 bg-grey-700 hover:bg-gray-600 focus:ring-gray-600 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-500 text-sm font-medium px-5 py-2.5 hover:text-white focus:z-10"
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
+              </svg>
+              <span className="sr-only">Previous</span>
+            </span>
+          </button>
+          <button
+            type="button"
+            className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            data-carousel-next
+          >
+            <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 text-gray-400 bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 hover:text-white">
+              <svg
+                aria-hidden="true"
+                className="w-10 h-10 sm:w-10 sm:h-10 text-gray:100"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                Decline
-              </button>
-            </div>
-          </div>
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+              </svg>
+              <span className="sr-only">Next</span>
+            </span>
+          </button>
+
+          <p className="px-20 text-center">{images[selectedImageIndex]?.caption}</p>
+          <p className="px-20 text-center italic">
+            Photo {selectedImageIndex + 1} of {images.length}
+          </p>
         </div>
       </div>
     </div>
--- src/components/ParkSections/Photos.tsx
+++ src/components/ParkSections/Photos.tsx
@@ -1,18 +1,18 @@
 // React
-import { FC, useState } from "react";
+import { FC, useState } from 'react';
 
 // Next
-import Image from "next/image";
+import Image from 'next/image';
 
 // UUID
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from 'uuid';
 
 // Components
-import ParkInfoSection from "../ParkInfoSection";
-import ImageModal from "../Modals/ImageModal";
+import ParkInfoSection from '../ParkInfoSection';
+import ImageModal from '../Modals/ImageModal';
 
 // Custom Types
-import { IParkImage } from "../../customTypes/parks";
+import { IParkImage } from '../../customTypes/parks';
 
 interface IProps {
   images: IParkImage[];
@@ -20,30 +20,36 @@ interface IProps {
 
 const Photos: FC<IProps> = ({ images }) => {
   const [showModal, setShowModal] = useState(false);
-  const [selectedImage, setSelectedImage] = useState<IParkImage | undefined>(undefined);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
-  const handleImageClick = (image: IParkImage) => {
-    setSelectedImage(image);
+  const handleImageClick = (index: number) => {
+    setSelectedImageIndex(index);
     setShowModal(true);
   };
 
   return (
     <ParkInfoSection title="Photos">
       <ul className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
-        {images.map((image) => {
+        {images.map((image, index) => {
           return (
             <li
-              onClick={() => handleImageClick(image)}
+              onClick={() => handleImageClick(index)}
               key={uuidv4()}
               className="2xl:h-96 xl:h-96 lg:h-80 md:h-72 sm:h-64 h-64 cursor-pointer"
-              style={{ position: "relative", width: "100%" }}
+              style={{ position: 'relative', width: '100%' }}
             >
               <Image blurDataURL={image.url} src={image.url} alt={image.altText} layout="fill" objectFit="cover" />
             </li>
           );
         })}
       </ul>
-      <ImageModal showModal={showModal} setShowModal={setShowModal} parkImg={selectedImage} />
+      <ImageModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        images={images}
+        selectedImageIndex={selectedImageIndex}
+        setSelectedImageIndex={setSelectedImageIndex}
+      />
     </ParkInfoSection>
   );
 };
